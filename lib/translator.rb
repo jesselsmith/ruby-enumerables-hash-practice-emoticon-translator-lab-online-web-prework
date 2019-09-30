b966072dbe5ea5137fc7f583a3b2c5a0db39a2c5
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-30 04:24:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,15 +5,18 @@ def load_library(path)
   # code goes here
   library = YAML.load_file(path)
   
-  get_meaning = library.reduce({}) { |memo, (key, value)|
+emoticon_dictionary = {
+  
+  :get_meaning => library.reduce({}) { |memo, (key, value)|
      memo[value[1]] = key
+     memo
   }
   
-  get_emoticon = library.reduce({}){ |memo, (key, value)|
-    memo[value[0]] = key
+  :get_emoticon => library.reduce({}){ |memo, (key, value)|
+    memo[value[0]] = value[1]
+    memo
   }
-  
-
+}
 end
 
 def get_japanese_emoticon
